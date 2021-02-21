3f0a695494f2cf5c1612e553af39d38031bb0b2c
mcrowther <mcrowther@localhost.localdomain>
2021-02-21 17:44:29 +0000

    Modification of gitignore

--- load_graphs.sh
+++
@@ -1,9 +0,0 @@
-
-
-python3.6 download_ontologies.py
-sudo /usr/local/bin/docker-compose up
-
-#sudo docker cp ../graph_store/ontologies/ 794e1609b99e:/data
-#sudo docker exec -it server_db_1 /bin/bash 
-#isql-v -U dba
-
